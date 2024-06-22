4c918548f0eeb9faf227ef2f2ec6fa7bd38ca146
Moritz Körber <koerber.moritz@gmail.com>
2024-06-22 14:24:04 +0200

    Add do_something

--- v2/function_app.py
+++ v2/function_app.py
@@ -1,8 +1,10 @@
 import azure.functions as func
 from copy_blobs.copy_blobs import bp as copy_blobs_func
+from do_something_blob.do_something_blob import bp as do_something_blob_func
 from http_trigger.http_trigger import bp as http_trigger_func
 
 app = func.FunctionApp()
 
 app.register_functions(copy_blobs_func)
 app.register_functions(http_trigger_func)
+app.register_functions(do_something_blob_func)
