08e903f849479da11466dc2e815387a31ec0d743
Moritz Körber <koerber.moritz@gmail.com>
2024-06-22 13:48:17 +0200

    Add working copy_blobs

--- v2/copy_blobs/copy_blobs.py
+++ v2/copy_blobs/copy_blobs.py
@@ -11,20 +11,18 @@ bp = func.Blueprint()
 @bp.blob_trigger(
     arg_name="obj",
     path="mycontainer/{name}.zip",  # only copy zip files
-    connection="AZURE_STORAGE_SOURCE_CONN_STR",
+    connection="StorageConnectionString",
 )
 @bp.blob_input(
     arg_name="inputblob",
     path="mycontainer/{name}.zip",
-    connection="AZURE_STORAGE_SOURCE_CONN_STR",
+    connection="StorageConnectionString",
 )
 @bp.blob_output(
     arg_name="outputblob",
     path="myothercontainer/{name}.zip",
-    connection="AZURE_STORAGE_DEST_CONN_STR",
+    connection="StorageConnectionString",
 )
-def blob_trigger_function(
-    obj: func.InputStream, inputblob: bytes, outputblob: func.Out[bytes]
-):
-    logging.info("Python Queue trigger function processed %s", obj.name)
+def copy_blobs(obj: func.InputStream, inputblob: bytes, outputblob: func.Out[bytes]):
+    logging.info("Processed %s", obj.name)
     outputblob.set(inputblob)
