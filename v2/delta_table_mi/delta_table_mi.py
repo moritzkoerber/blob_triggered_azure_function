d6f7290efe0db5a30d325a344ef7f2ebbf5eb514
Moritz Körber <koerber.moritz@gmail.com>
2024-06-23 12:22:49 +0200

    Add delta table using managed identity

--- v2/delta_table_mi/delta_table_mi.py
+++ v2/delta_table_mi/delta_table_mi.py
@@ -11,17 +11,17 @@ bp = func.Blueprint()
 
 
 @bp.blob_trigger(
-    arg_name="blob", path="mycontainer/{name}.parquet", connection="STA_CONN_STRING"
+    arg_name="blob", path="mycontainer/delta/{name}.parquet", connection="STA_MI_CONN"
 )
-def delta_table_func(blob: func.InputStream):
+def delta_table_func_mi(blob: func.InputStream):
     # type(blob.read()) is bytes
     df = pl.read_parquet(io.BytesIO(blob.read()))
     df.write_delta(
-        "abfss://test/delta_table",
+        "abfss://test/test_mi/delta_table",
         mode="overwrite",
         storage_options={
             "account_name": os.environ["AZ_STA_NAME"],
-            "account_key": os.environ["AZ_STA_KEY"],
+            "azure_msi_endpoint": os.environ["IDENTITY_ENDPOINT"],
         },
     )
     print(df)
